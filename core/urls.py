68553df763546e3609e14e54844d17b0e0cf3db1
Sayemahamed <sayemahamed183@gmail.com>
2024-11-02 13:35:10 +0600

    added UI pages

--- core/urls.py
+++ core/urls.py
@@ -3,7 +3,7 @@ from django.urls.resolvers import URLPattern
 
 from core import views
 
-
+app_name = "core"
 urlpatterns: list[URLPattern] = [
     path(route="", view=views.index, name="index"),
     path("accounts/profile/", views.profile, name="profile"),
