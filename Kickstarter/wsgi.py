f81eadb585dd8721544cebc6bc3f42b1cc6a6ac9
Sayemahamed <sayemahamed183@gmail.com>
2024-11-19 22:02:20 +0600

    changed project name from all__auth to DjangoKickstarter

--- Kickstarter/wsgi.py
+++ Kickstarter/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for all__auth project.
+WSGI config for Kickstarter project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "all__auth.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Kickstarter.settings")
 
 application = get_wsgi_application()
--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "all__auth.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Kickstarter.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
