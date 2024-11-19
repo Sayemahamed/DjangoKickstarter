f86db75d5b270691fd4256aefd516b03167da276
Sayemahamed <sayemahamed183@gmail.com>
2024-11-19 22:16:39 +0600

    implementing theming using tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./templates/**/**/*.html"],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: 'rgb(var(--color-primary) / <alpha-value>)',
+        secondary: 'rgb(var(--color-secondary) / <alpha-value>)',
+        accent: 'rgb(var(--color-accent) / <alpha-value>)',
+        text: 'rgb(var(--color-text) / <alpha-value>)',
+      }
+    },
   },
   plugins: [],
 }
-
