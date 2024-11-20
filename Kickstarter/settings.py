d59bae103fbeaf36f7c417d34b448c5792c2ec5a
Sayemahamed <sayemahamed183@gmail.com>
2024-11-20 14:18:00 +0600

    properly structured settings file

--- Kickstarter/settings.py
+++ Kickstarter/settings.py
@@ -12,42 +12,29 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 import os
 from pathlib import Path
+from decouple import config, Csv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR: Path = Path(__file__).resolve().parent.parent
-TEMPLATES_DIR: str = os.path.join(BASE_DIR, "templates")
-MEDIA_ROOT: str = os.path.join(BASE_DIR, "media")
-STATICFILES_DIRS: list[str] = [
-    os.path.join(BASE_DIR, "static"),
-    # "/var/www/static",
-]
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
+# SECURITY SETTINGS
+# ------------------------------------------------------------------------------
+SECRET_KEY = config('DJANGO_SECRET_KEY', default='django-insecure-key')
+DEBUG = config('DEBUG', default=False, cast=bool)
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1', cast=Csv())
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "@xrl+$k(l%pu4l89$+rro&ex7uu3+!n682rj(978dlm0dt297f"
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-
-ALLOWED_HOSTS: list[str] = ["localhost", "127.0.0.1"]
-ACCOUNT_AUTHENTICATION_METHOD: str = "email"
-ACCOUNT_EMAIL_REQUIRED: bool = True
-ACCOUNT_EMAIL_VERIFICATION: str = "optional"
-ACCOUNT_USERNAME_REQUIRED = False
-# Application definition
-
-INSTALLED_APPS: list[str] = [
+# APPS
+# ------------------------------------------------------------------------------
+DJANGO_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    # my apps
-    "core",
-    # package apps
+]
+
+THIRD_PARTY_APPS = [
     "allauth",
     "allauth.account",
     "allauth.socialaccount",
@@ -57,7 +44,15 @@ INSTALLED_APPS: list[str] = [
     "django_browser_reload",
 ]
 
-MIDDLEWARE: list[str] = [
+LOCAL_APPS = [
+    "core",
+]
+
+INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
+
+# MIDDLEWARE
+# ------------------------------------------------------------------------------
+MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -65,12 +60,18 @@ MIDDLEWARE: list[str] = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    # package middleware
     "allauth.account.middleware.AccountMiddleware",
     "django_browser_reload.middleware.BrowserReloadMiddleware",
 ]
-ROOT_URLCONF = "Kickstarter.urls"
 
+# URLS
+# ------------------------------------------------------------------------------
+ROOT_URLCONF = "Kickstarter.urls"
+WSGI_APPLICATION = "Kickstarter.wsgi.application"
+
+# TEMPLATES
+# ------------------------------------------------------------------------------
+TEMPLATES_DIR = os.path.join(BASE_DIR, "templates")
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
@@ -87,47 +88,8 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "Kickstarter.wsgi.application"
-
-AUTHENTICATION_BACKENDS: list[str] = [
-    "django.contrib.auth.backends.ModelBackend",
-    "allauth.account.auth_backends.AuthenticationBackend",
-]
-# EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
-
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 465  # Use SSL
-EMAIL_USE_SSL = True  # Use SSL instead of TLS
-EMAIL_HOST_USER = 'allauthtest15@gmail.com'
-EMAIL_HOST_PASSWORD = 'pymvyuccgixixlyk' 
-DEFAULT_FROM_EMAIL = 'allauthtest15@gmail.com'
-
-
-SOCIALACCOUNT_FORMS = {
-    "disconnect": "allauth.socialaccount.forms.DisconnectForm",
-    "signup": "allauth.socialaccount.forms.SignupForm",
-}
-SOCIALACCOUNT_PROVIDERS = {
-    "google": {
-        "SCOPE": [
-            "profile",
-            "email",
-        ],
-        "AUTH_PARAMS": {
-            "access_type": "online",
-        },
-        "OAUTH_PKCE_ENABLED": True,
-    },
-    "facebook": {
-        "METHOD": "oauth2",
-        "SCOPE": ["email", "public_profile"],
-        "AUTH_PARAMS": {"auth_type": "online"},
-    },
-}
-# Database
-# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
+# DATABASES
+# ------------------------------------------------------------------------------
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
@@ -135,45 +97,94 @@ DATABASES = {
     }
 }
 
-
-# Password validation
-# https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
-
-AUTH_PASSWORD_VALIDATORS: list[dict[str, str]] = [
-    {
-        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
-    },
-    {
-        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
-    },
-    {
-        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
-    },
-    {
-        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
-    },
+# AUTHENTICATION
+# ------------------------------------------------------------------------------
+AUTH_PASSWORD_VALIDATORS = [
+    {"NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"},
+    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator"},
+    {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator"},
+    {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
 ]
 
+AUTHENTICATION_BACKENDS = [
+    "django.contrib.auth.backends.ModelBackend",
+    "allauth.account.auth_backends.AuthenticationBackend",
+]
 
-# Internationalization
-# https://docs.djangoproject.com/en/5.1/topics/i18n/
+# ALLAUTH SETTINGS
+# ------------------------------------------------------------------------------
+ACCOUNT_AUTHENTICATION_METHOD = "email"
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_EMAIL_VERIFICATION = "optional"
+ACCOUNT_USERNAME_REQUIRED = False
 
+# INTERNATIONALIZATION
+# ------------------------------------------------------------------------------
 LANGUAGE_CODE = "en-us"
-
 TIME_ZONE = "UTC"
-
 USE_I18N = True
-
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.1/howto/static-files/
-
+# STATIC FILES
+# ------------------------------------------------------------------------------
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
+# MEDIA FILES
+# ------------------------------------------------------------------------------
 MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+# EMAIL
+# ------------------------------------------------------------------------------
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 465
+EMAIL_USE_SSL = True
+EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default=EMAIL_HOST_USER)
+
+# SOCIAL ACCOUNT
+# ------------------------------------------------------------------------------
+SOCIALACCOUNT_FORMS = {
+    "disconnect": "allauth.socialaccount.forms.DisconnectForm",
+    "signup": "allauth.socialaccount.forms.SignupForm",
+}
+
+SOCIALACCOUNT_PROVIDERS = {
+    "google": {
+        "APP": {
+            "client_id": config('GOOGLE_CLIENT_ID', default=''),
+            "secret": config('GOOGLE_CLIENT_SECRET', default=''),
+            "key": "",
+        },
+        "SCOPE": [
+            "profile",
+            "email",
+        ],
+        "AUTH_PARAMS": {
+            "access_type": "online",
+        },
+    },
+    "facebook": {
+        "APP": {
+            "client_id": config('FACEBOOK_CLIENT_ID', default=''),
+            "secret": config('FACEBOOK_CLIENT_SECRET', default=''),
+            "key": "",
+        },
+        "SCOPE": [
+            "email",
+            "public_profile",
+        ],
+        "AUTH_PARAMS": {"auth_type": "reauthenticate"},
+        "METHOD": "oauth2",
+    },
+}
 
 # Default primary key field type
-# https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
-
+# ------------------------------------------------------------------------------
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
